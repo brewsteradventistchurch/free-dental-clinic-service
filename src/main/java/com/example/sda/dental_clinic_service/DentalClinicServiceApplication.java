3988dc7d5950af2146df3f267de9bf77da9d9e68
brewsteradventistchurch <brewster.adventist.church@gmail.com>
2026-08-03 17:11:35 -0700

    initial commit

---
+++ src/main/java/com/example/sda/dental_clinic_service/DentalClinicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sda.dental_clinic_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalClinicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalClinicServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sda/dental_clinic_service/DentalClinicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sda.dental_clinic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
